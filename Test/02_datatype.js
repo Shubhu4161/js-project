8a8bb18599887ff2ac00ef5d9a7c28be14c44751
Shubham <151820245+Shubhu4161@users.noreply.github.com>
2024-04-22 06:42:39 +0000

    container config done

---
+++ Test/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+// alert("") // we are using nodejs, not browser
+
+console.log(3+3)
+
+
+
+console.log("shuhbham")
+
+
+
+let name ="shubham"
+let age = 18
+let isLoggedId = false
+
+// number => 2 to power 53
+//bigint
+// string => ""
+//boolean => true/ false
+// null =>  standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "shubham");
---
+++ Test/03_conversionOperation.js
@@ -0,0 +1,9 @@
+let score = 44
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber);
