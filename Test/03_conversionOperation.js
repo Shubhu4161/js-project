96029d06cf4d588ef990533fda970b484b40ca87
Shubham <151820245+Shubhu4161@users.noreply.github.com>
2024-04-22 06:55:52 +0000

    container config done

--- Test/03_conversionOperation.js
+++ Test/03_conversionOperation.js
@@ -1,9 +1,27 @@
-let score = 44
+let score = true
 
 console.log(typeof score);
 console.log(typeof(score));
 
 
 
-let valueNumber = Number(score)
-console.log(typeof valueNumber);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true=> 1; false => 0
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
